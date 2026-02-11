a04e4135d1c617f0ce96deaf3e4c41a4609333f1
ballisingha <guriqbal@hotmail.de>
2026-02-11 22:06:18 +0100

    Done the Challene

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var appChoices = ["Rock", "Paper", "Scissors"]
+    @State private var appsChoice = Int.random(in: 0...2)
+    let playerChoices = ["Rock", "Paper", "Scissors"]
+    @State private var score = 0
+    @State private var showingAlert = ""
+    @State private var showingScore = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("Let´s play Rock, Paper, Scissors!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Your score is \(score)")
+                .font(.title2)
+                .padding()
+          
+            VStack {
+                ForEach(0 ..< 3) { number in
+                    Button {
+                        buttonTapped(number)
+                    } label: {
+                        ButtonImage(number)
+                    }
+                }
+            }.alert(showingAlert, isPresented: $showingScore) {
+                Button("Continue", action: shuffledChoices)
+            } message: {
+                Text("Your score is \(score) the app chose\(appChoices[appsChoice])")
+                Text("")
+            }
+
         }
         .padding()
+    }
+    func shuffledChoices() {
+        appChoices.shuffle()
+        appsChoice = Int.random(in: 0...2)
+        showingScore = false
+    }
+    func buttonTapped(_ number: Int) {
+        if number == appsChoice {
+            showingAlert = "It´s a draw!"
+            showingScore = true
+        } else if (number == 0 && appsChoice == 2) || (number == 1 && appsChoice == 0) || (number == 2 && appsChoice == 1) {
+            showingAlert = "You win!"
+            score += 1
+            showingScore = true
+        } else {
+            showingAlert = "You lose!"
+            score -= 1
+            showingScore = true
+        }
+    }
+    func ButtonImage(_ number: Int) -> some View {
+        Text(playerChoices[number])
+            .font(.title)
+            .padding(.vertical, 12)
+            .padding(.horizontal, 20)
+            .background(.thinMaterial, in: Capsule())
+            .shadow(radius: 5)
     }
 }
 
